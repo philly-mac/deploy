ea6750e2573012d9f4d8e5106e9e768b24c7e234
Philip MacIver <philip.maciver@xing.com>
2010-10-18 17:22:50 +0200

    Added config code

--- lib/deploy/config.rb
+++ lib/deploy/config.rb
@@ -2,17 +2,40 @@ module Deploy
   class Config
     attr_accessor :env
     attr_accessor :user_name
+    attr_accessor :deploy_root
+    attr_accessor :app_root
     attr_accessor :remote
+    attr_accessor :current_path
+    attr_accessor :shared_path
+    attr_accessor :release_path
 
     def initialize
-      self.deploy_root   "/var/www"
-      self.app_root      "test"
-      self.current_path  "#{self.deploy_root}#{self.app_root}/current"
-      self.shared_path   "#{self.deploy_root}#{self.app_root}/shared"
-      self.release_path  "#{self.deploy_root}#{self.app_root}/releases"
+      set :deploy_root,   "/var/www"
+      set :app_root,      "test"
+      set :current_path,  "#{self.deploy_root}#{self.app_root}/current"
+      set :shared_path,   "#{self.deploy_root}#{self.app_root}/shared"
+      set :release_path,  "#{self.deploy_root}#{self.app_root}/releases"
+    end
+
+    def environment_config
+      file = "#{APP_ROOT}/config/#{self.env}.rb"
+      if File.exists?(file)
+        file_contents = ""
+        File.open(file, "r") do |infile|
+          while (line = infile.gets)
+            file_contents += line
+          end
+        end
+        eval file_contents
+      end
+    end
+
+    def set(key, value)
+      self.send("#{key.to_s}=".to_sym, value)
     end
 
     def method_missing(method_name, *args, &block)
+      method_name = method_name.to_s.gsub('=','')
       ::Deploy::Config.send :attr_accessor, method_name.to_sym
       self.send(:"#{method_name}=", *args)
     end
--- lib/deploy/recipies/rails.rb
+++ lib/deploy/recipies/rails.rb
@@ -5,13 +5,16 @@ module Deploy
       extend ::Deploy::Base
 
       class << self
+        attr_accessor :config
 
         def first_run(config)
+          self.config = config
           create_directories
           setup_db
         end
 
         def deploy(config)
+          self.config = config
           get_code
           release_dir
           unpack
@@ -25,18 +28,18 @@ module Deploy
         private
 
         def create_directories
-          create_directory "#{shared_path}/log"
-          create_directory "#{shared_path}/db"
-          create_directory "#{shared_path}/system"
-          create_directory "#{shared_path}/config"
-          create_directory "#{shared_path}/config/monit.d"
-          create_directory "#{shared_path}/config/hostapd.d"
-          create_directory "#{shared_path}/config/dnsmasq.d"
-          create_directory "#{shared_path}/config/ifconfig.d"
-          create_directory "#{shared_path}/solr/data"
-          create_directory "#{shared_path}/user-files", 0770
-          create_directory "#{shared_path}/pids", 0770
-          create_directory "#{shared_path}/avatars", 0770
+          create_directory "#{config.shared_path}/log"
+          create_directory "#{config.shared_path}/db"
+          create_directory "#{config.shared_path}/system"
+          create_directory "#{config.shared_path}/config"
+          create_directory "#{config.shared_path}/config/monit.d"
+          create_directory "#{config.shared_path}/config/hostapd.d"
+          create_directory "#{config.shared_path}/config/dnsmasq.d"
+          create_directory "#{config.shared_path}/config/ifconfig.d"
+          create_directory "#{config.shared_path}/solr/data"
+          create_directory "#{config.shared_path}/user-files", 0770
+          create_directory "#{config.shared_path}/pids", 0770
+          create_directory "#{config.shared_path}/avatars", 0770
         end
 
         def create_directory(dir_name, permissions = nil)
@@ -45,7 +48,7 @@ module Deploy
         end
 
         def setup_db
-          FileUtils.cd current_path
+          FileUtils.cd config.current_path
           system "mysql -u root dashboard_production -e 'show tables;' 2>&1 > /dev/null"
           if $?.exitstatus != 0
             system "RAILS_ENV=#{self.env} bundle exec rake db:setup"
@@ -53,37 +56,37 @@ module Deploy
         end
 
         def release_dir
-          FileUtils.mkdir_p shared_path if !File.exists? shared_path
-          FileUtils.mkdir_p releases_path if !File.exists? releases_path
+          FileUtils.mkdir_p config.shared_path if !File.exists? config.shared_path
+          FileUtils.mkdir_p config.releases_path if !File.exists? config.releases_path
         end
 
         def unpack
           if File.exists?("/tmp/#{ARCHIVE_NAME}#{PACKING_TYPE}")
             FileUtils.cd "/tmp"
             system "tar -xzf #{ARCHIVE_NAME}#{PACKING_TYPE}"
-            FileUtils.mv ARCHIVE_NAME, "#{release_path}/#{Time.now.strftime('%Y%m%d%H%M%S')}"
+            FileUtils.mv ARCHIVE_NAME, "#{config.release_path}/#{Time.now.strftime('%Y%m%d%H%M%S')}"
           end
         end
 
         def clean_up
-          all_releases = Dir["#{release_path}/*"].sort
+          all_releases = Dir["#{config.release_path}/*"].sort
           if (num_releases = all_releases.size) > MAX_NUM_RELEASES
             num_to_delete = num_releases - MAX_NUM_RELEASES
 
             num_to_delete.times do
-              FileUtils.r_rf "#{release_path}/#{all_releases.delete_at(0)}"
+              FileUtils.r_rf "#{config.release_path}/#{all_releases.delete_at(0)}"
             end
           end
         end
 
         def bundle
-          shared_dir = File.join(shared_path, 'bundle')
-          release_dir = File.join(release_path, '.bundle')
+          shared_dir = File.join(config.shared_path, 'bundle')
+          release_dir = File.join(config.release_path, '.bundle')
 
           FileUtils.mkdir_p shared_dir
           FileUtils.ln_s shared_dir, release_dir
 
-          FileUtils.cd release_path
+          FileUtils.cd config.release_path
 
           system "bundle check 2>&1 > /dev/null"
 
@@ -93,17 +96,17 @@ module Deploy
         end
 
         def migrate
-          FileUtils.cd current_path
-          system "RAILS_ENV=#{env} rake db:migrate"
+          FileUtils.cd config.current_path
+          system "RAILS_ENV=#{config.env} rake db:migrate"
         end
 
         def link
           FileUtils.rm current_path
-          FileUtils.ln_s latest_deploy, current_path
+          FileUtils.ln_s latest_deploy, config.current_path
         end
 
         def restart
-          FileUtils.touch "#{current_path}/tmp/restart.txt"
+          FileUtils.touch "#{config.current_path}/tmp/restart.txt"
         end
       end
     end
