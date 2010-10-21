c04b861b1376fcc13a163d4d33a760a0130d4328
Ali Jelveh <jelveh@web.de>
2010-10-21 17:52:37 +0200

    output

--- lib/deploy/recipies/protonet.rb
+++ lib/deploy/recipies/protonet.rb
@@ -63,6 +63,7 @@ module Deploy
           FileUtils.cd latest_deploy do
             db_exists = system("mysql -u root #{config.database_name} -e 'show tables;' 2>&1 > /dev/null")
             if !db_exists
+              puts 'db not found, creating'
               system "RAILS_ENV=#{config.env} bundle exec rake db:setup"
             end
           end
