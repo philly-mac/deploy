5699144702f2182bbfb70c26e3eaec84b2736384
Philip MacIver <philip.maciver@xing.com>
2010-11-11 11:08:55 +0100

    some tweeks

--- lib/deploy/base.rb
+++ lib/deploy/base.rb
@@ -20,8 +20,8 @@ module Deploy
         r_commands = self.remote_commands.map do |r_command|
           puts "REMOTE: #{r_command}" if ::Deploy::Deploy.verbose
           r_command
-        end.join(" && ")
-        local "ssh #{config.extra_ssh_options} #{config.username}@#{config.remote} #{r_commands}"
+        end.join("; ")
+        local "ssh #{config.extra_ssh_options} #{config.username}@#{config.remote} '#{r_commands}'"
         puts "\n"
         self.remote_commands = []
       end
