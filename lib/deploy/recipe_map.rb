919ffd04e490255076d635845a661699a02f105c
Philip MacIver <philip.maciver@xing.com>
2010-12-08 15:54:18 +0100

    Added recipe map and incremented the version number

--- lib/deploy/recipe_map.rb
+++ lib/deploy/recipe_map.rb
@@ -4,15 +4,13 @@ module Deploy
       attr_accessor :recipe_map
 
       def map(clazz_name, aliaz)
-        recipe_map ||= {}
-
-        recipe_map[aliaz.to_s] = clazz_name.to_s
+        self.recipe_map ||= {}
+        self.recipe_map[aliaz.to_s] = clazz_name.to_s
       end
 
       def recipe_clazz(clazz_name_or_aliaz)
         clazz_name_or_aliaz = clazz_name_or_aliaz.to_s
-        return recipe_map[clazz_name_or_aliaz] if recipe_map.has_key?(clazz_name_or_aliaz)
-        clazz_name_or_aliaz
+        return recipe_map.has_key?(clazz_name_or_aliaz) ? recipe_map[clazz_name_or_aliaz] : clazz_name_or_aliaz
       end
     end
   end
